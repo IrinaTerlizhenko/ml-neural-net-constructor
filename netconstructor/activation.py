91757c571e429ded79091bfd2604d2a7a5dc78c7
Algis Ivashkiavichus <Algis_Ivashkiavichus@epam.com>
2019-04-04 01:51:58 +0300

    BatchNorm and Activations
    Add BatchNorm layer
    Add LeakyRelu activation
    Add Elu activation
    Add ParamRelu activation

--- netconstructor/activation.py
+++ netconstructor/activation.py
@@ -1,11 +1,9 @@
-# todo: add parameter for degree
 import numpy as np
 
 from netconstructor.layer import Layer
 
 
 class ReluActivation(Layer):
-
     def __init__(self, num_neurons: int) -> None:
         super().__init__()
         self._num_neurons = num_neurons
@@ -13,7 +11,7 @@ class ReluActivation(Layer):
         self._current_output: np.ndarray = None
 
     def propagate(self, x: np.ndarray) -> np.ndarray:
-        return np.max(x, 0)
+        return np.max(x, 0)  # FIXME: SHOULD WE SAVE CURRENT OUTPUT? ALSO SECOND PARAM IS AXIS AND NOT MAX_VALUE
 
     def back_propagate(self, dx: np.ndarray) -> np.ndarray:
         relu_dx = self._current_output > 0  # ones when dx > 0 and zeros otherwise
@@ -25,7 +23,6 @@ class ReluActivation(Layer):
 
 
 class LogisticActivation(Layer):
-
     THRESHOLD = -10
 
     def __init__(self, num_neurons: int) -> None:
@@ -43,6 +40,87 @@ class LogisticActivation(Layer):
     def back_propagate(self, dx: np.ndarray) -> np.ndarray:
         logistic_dx = self._current_output * (1 - self._current_output)  # component-wise
         return logistic_dx * dx  # component-wise
+
+    @property
+    def num_outputs(self):
+        return self._num_neurons
+
+
+class LeakyReluActivation(Layer):
+    def __init__(self, num_neurons: int, alpha: float = 0.01) -> None:
+        assert 0 < alpha < 0.5, "alpha should be between 0 and 0.5"
+        super().__init__()
+        self._num_neurons = num_neurons
+        self._alpha = alpha
+
+        self._feed_func = np.vectorize(lambda x: x if x > 0. else self._alpha * x)
+        self._back_func = np.vectorize(lambda x: 1. if x > 0. else self._alpha)
+
+        self._current_output: np.ndarray = None
+
+    def propagate(self, x: np.ndarray) -> np.ndarray:
+        self._current_output = self._feed_func(x)
+        return self._current_output
+
+    def back_propagate(self, dx: np.ndarray) -> np.ndarray:
+        relu_dx = self._back_func(self._current_output)
+        return relu_dx * dx
+
+    @property
+    def num_outputs(self):
+        return self._num_neurons
+
+
+class ParamReluActivation(Layer):
+    def __init__(self, num_neurons: int, learning_rate: float, alpha: float = 0.01) -> None:
+        assert 0 < alpha < 0.5, "alpha should be between 0 and 0.5"
+        super().__init__()
+        self._num_neurons = num_neurons
+        self._learning_rate = learning_rate
+
+        self._alpha = alpha
+
+        self._feed_func = np.vectorize(lambda x: x if x > 0. else self._alpha * x)
+        self._back_func = np.vectorize(lambda x: 1. if x > 0. else self._alpha)
+
+        self._current_input: np.ndarray = None
+        self._current_output: np.ndarray = None
+
+    def propagate(self, x: np.ndarray) -> np.ndarray:
+        self._current_input = x.copy()
+        self._current_output = self._feed_func(x)
+        return self._current_output
+
+    def back_propagate(self, dx: np.ndarray) -> np.ndarray:
+        dalpha = np.sum(np.clip(self._current_input, a_min=None, a_max=0.) * dx, axis=0)
+        relu_dx = self._back_func(self._current_output)
+        self._alpha -= self._learning_rate * dalpha  # TODO: SHOULD WE TAKE AVG OR UPDATE EVERY ELEMENT?
+        return relu_dx * dx
+
+    @property
+    def num_outputs(self):
+        return self._num_neurons
+
+
+class EluActivation(Layer):
+    def __init__(self, num_neurons: int, alpha: float = 0.01) -> None:
+        assert 0 <= alpha, "alpha should be not less than 0"
+        super().__init__()
+        self._num_neurons = num_neurons
+        self._alpha = alpha
+
+        self._feed_func = np.vectorize(lambda x: x if x > 0. else self._alpha * (np.e ** x - 1.))
+        self._back_func = np.vectorize(lambda x: 1. if x > 0. else self._alpha * (np.e ** x))
+
+        self._current_output: np.ndarray = None
+
+    def propagate(self, x: np.ndarray) -> np.ndarray:
+        self._current_output = self._feed_func(x)
+        return self._current_output
+
+    def back_propagate(self, dx: np.ndarray) -> np.ndarray:
+        relu_dx = self._back_func(self._current_output)
+        return relu_dx * dx
 
     @property
     def num_outputs(self):
--- netconstructor/layer.py
+++ netconstructor/layer.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 class Layer(ABC):
-
     @abstractmethod
     def propagate(self, x: np.ndarray) -> np.ndarray:
         pass
@@ -20,7 +19,6 @@ class Layer(ABC):
 
 
 class DenseLayer(Layer):
-
     def __init__(self, num_inputs: int, num_outputs: int, learning_rate: float, initial_weights: np.ndarray = None,
                  initial_biases: np.ndarray = None) -> None:
         self._num_inputs = num_inputs
@@ -50,6 +48,55 @@ class DenseLayer(Layer):
         # so we need just to repeat diff_weights _num_outputs times and shape it correctly
         diff_weights = np.tile(diff_weights, self._num_outputs).reshape((self._num_outputs, self._num_inputs)).T
         self._weight -= self._learning_rate * diff_weights
+
+        return output_dx
+
+    @property
+    def num_outputs(self):
+        return self._num_outputs
+
+
+class BatchNorm(Layer):
+    EPSILON = 0.001
+
+    def __init__(self, num_outputs: int, learning_rate: float, gamma: float = 1,
+                 beta: float = 0) -> None:  # TODO: IS_TRAIN (MOVING MEAN AND AVG)
+        self._num_outputs = num_outputs
+
+        self._learning_rate = learning_rate
+
+        self._gamma = gamma
+        self._beta = beta
+
+        self._current_inputs: np.ndarray = None
+
+    def propagate(self, x: np.ndarray) -> np.ndarray:
+        self._current_inputs = x.copy()
+
+        batch_size = x.shape[0]
+        mu = 1 / batch_size * np.sum(x, axis=0)
+        sigma2 = 1 / batch_size * np.sum((x - mu) ** 2, axis=0)
+        hath = (x - mu) * (sigma2 + BatchNorm.EPSILON) ** (-1. / 2.)
+
+        return self._gamma * hath + self._beta
+
+    def back_propagate(self, dx: np.ndarray) -> np.ndarray:
+        x = self._current_inputs
+        batch_size = x.shape[0]
+        mu = 1. / batch_size * np.sum(x, axis=0)
+        var = 1. / batch_size * np.sum((x - mu) ** 2, axis=0)
+        dbeta = np.sum(dx, axis=0)
+        dgamma = np.sum((x - mu) * (var + BatchNorm.EPSILON) ** (-1. / 2.) * dx, axis=0)
+        output_dx = (1. / batch_size) * self._gamma * (var + BatchNorm.EPSILON) ** (-1. / 2.) \
+                    * (
+                        batch_size * dx - np.sum(dx, axis=0)
+                        - (x - mu)
+                        * (var + BatchNorm.EPSILON) ** (-1.0)
+                        * np.sum(dx * (x - mu), axis=0)
+                    )
+
+        self._beta -= self._learning_rate * dbeta  # TODO: SHOULD WE TAKE AVG OR UPDATE EVERY ELEMENT?
+        self._gamma -= self._learning_rate * dgamma
 
         return output_dx
 
